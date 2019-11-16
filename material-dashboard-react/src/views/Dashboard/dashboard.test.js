55461ffd594314920d52f8636f84f05e838e3284
AshuGuptaCollege <ashuguptacollege@gmail.com>
2019-11-16 12:13:29 -0600

    render part 3203983084

--- material-dashboard-react/src/views/Dashboard/dashboard.test.js
+++ material-dashboard-react/src/views/Dashboard/dashboard.test.js
@@ -5,8 +5,10 @@ import Dashboard from './Dashboard.js'
 
 test('Dashboard should render passed props as content body and respond to callback props', () => {
   const {getByTestId} = render(<Dashboard/>)
+  const {queryByTestId} = render(<Dashboard/>)
 
-  expect(getByTestId('studiesTable').textContent).not.toBeNull()
-  expect(getByTestId('upcomingStudies').textContent).toBe('Study IDtitletimerequirementpaymentlocationstart date/timeend date/time')
-  expect(getByTestId('pastStudies').textContent).toBe('Study IDtitletimerequirementpaymentlocationstart date/timeend date/time')
+  expect(queryByTestId('studiesTable')).toBeTruthy();
+  expect(queryByTestId('upcomingStudies')).toBeTruthy();
+  expect(getByTestId('upcomingStudies').textContent).toBe('Study IDtitletimerequirementpaymentlocationstart date/timeend date/time');
+  expect(queryByTestId('pastStudies')).not.toBeTruthy();
 })
