652c51ad0bba85d0716d0bda1df0cf634369e043
AshuGuptaCollege <ashuguptacollege@gmail.com>
2019-11-16 10:50:57 -0600

    trying to fix import issue

--- material-dashboard-react/src/views/Dashboard/dashboard.test.js
+++ material-dashboard-react/src/views/Dashboard/dashboard.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {render, getByText} from '@testing-library/react'
-import {toHaveTextContent} from '@testing-library/jest-dom/extend-expect'
+import {toHaveTextContent} from '@testing-library/jest-dom'
 import Dashboard from './Dashboard.js'
+
+expect.extend({toHaveTextContent, toHaveClass})
 
 test('Dashboard should render passed props as content body and respond to callback props', () => {
   const {dashboard} = render(<Dashboard/>)
