40bb17191a90842f26d80c20e2e4a06eef2e4aa8
AshuGuptaCollege <ashuguptacollege@gmail.com>
2019-11-27 13:51:16 -0600

    added simple display test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,10 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('Contains a table with past and upcoming studies', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=studiesTableCy]').should('contain', 'Upcoming Studies');
+    cy.get('[data-cy=studiesTableCy]').should('contain', 'Past Studies');
+  });
 });
