46e04a1a40c656245ace200429ddc9e13be1f963
AshuGuptaCollege <ashuguptacollege@gmail.com>
2019-12-08 18:24:34 -0600

    unbreaking the thing that was broken

--- material-dashboard-react/src/views/Dashboard/Dashboard.js
+++ material-dashboard-react/src/views/Dashboard/Dashboard.js
@@ -133,7 +133,7 @@ export default function Dashboard() {
                 )
               },
               {
-                tabName: "Past Studie",
+                tabName: "Past Studies",
                 tabIcon: ScheduleIcon,
                 tabContent: (
                 <Card data-testid="pastStudies">
